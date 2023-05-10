e8efebbe1b41a39bf2ca1f6f0005364015586086
ufodauge <hakka.ydy.sub@gmail.com>
2023-05-10 18:41:53 +0900

    main

--- src/data/csvtojson.py
+++ src/data/csvtojson.py
@@ -1,14 +1,16 @@
 import csv
+import os
 
 
 def main():
+    path = os.path.dirname(__file__)
     data = []
-    with open('tasklist.csv', 'r', encoding="utf-8") as f:
+    with open(path+'/tasklist.csv', 'r', encoding="utf-8") as f:
         reader = csv.reader(f)
 
         for row in reader:
             data.append({
-                "difficulty": row[2],
+                "difficulty": int(row[2]),
                 "contents": {
                     "ja": row[0],
                     "en": row[1]
@@ -16,7 +18,8 @@ def main():
                 "trackers": []
             })
 
-    print(data)
+    with open(path+"/temp.txt", "w", encoding="utf-8") as f:
+        f.write(str(data))
 
 
 main()
