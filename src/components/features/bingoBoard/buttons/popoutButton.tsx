5efab65f3d3d158f9a5dfa7d1bb4c4f249116d99
ufodauge <hakka.ydy.sub@gmail.com>
2023-11-02 03:42:52 +0900

    some updates

--- src/components/features/bingoBoard/buttons/popoutButton.tsx
+++ src/components/features/bingoBoard/buttons/popoutButton.tsx
@@ -1,24 +1,65 @@
-import { memo, ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, memo } from 'react';
 
-import Button from "@/components/ui/button";
-import { TASKS_QUERY_DELIMITER } from "@/const/popupWindowFeatures";
-import {
-  useBingoBoardActionsContext,
-  useBingoBoardValuesContext,
-} from "@/contexts/bingoBoard";
-import { useLanguageValue } from "@/contexts/language";
-import { useThemeValue } from "@/contexts/theme";
-import { CalcPopupWindowFeatures } from "@/lib/calcPopupWindowFeatures";
-import { useTaskData } from "@/lib/hooks/useTaskData";
-import { getTargetTasksByLineType } from "@/lib/utils/getTargetTasksByLineType";
-import { LineType } from "@/types/lineType";
-import { PopoutQuery } from "@/types/query/popout";
+import Button from '@/components/ui/button';
+import { useBingoBoardActionsContext, useBingoBoardValuesContext } from '@/contexts/bingoBoard';
+import { useLanguageValue } from '@/contexts/language';
+import { useThemeValue } from '@/contexts/theme';
+import { CalcPopupWindowFeatures } from '@/lib/calcPopupWindowFeatures';
+import { useTaskData } from '@/lib/hooks/useTaskData';
+import { LineType } from '@/types/lineType';
+import { PopoutQuery } from '@/types/query/popout';
 
-import * as style from "./popoutButton.css";
+import * as style from './popoutButton.css';
 
 type Props = {
   lineType: LineType;
 };
+
+const PopoutButton = memo<Props>(function PopoutButton({ lineType }) {
+  const { languageName }       = useLanguageValue();
+  const taskData               = useTaskData();
+  const { themeName }          = useThemeValue();
+  const { layout, seed }       = useBingoBoardValuesContext();
+  const { updateTargetedLine } = useBingoBoardActionsContext();
+
+  const repoName = taskData.repoName;
+
+  const url = `${isDevEnv ? "" : repoName}/popout`;
+  const params: PopoutQuery = {
+    seed    : seed.toString(),
+    header  : lineType,
+    layout  : lineType === "card" ? "card": layout,
+    lang    : languageName,
+    theme   : themeName,
+  };
+
+  const onClick = () => {
+    const features = CalcPopupWindowFeatures(
+      lineType === "card" ? "card" : layout,
+      taskData.size
+    );
+
+    const paramString = Object.entries(params)
+      .map(([k, v]) => `${k}=${v}`)
+      .join("&");
+    window.open(`${url}?${paramString}`, "_blank", features);
+  };
+
+  const onMouseOver = () => updateTargetedLine(lineType);
+  const onMouseOut = () => updateTargetedLine();
+
+  const customProps: ButtonHTMLAttributes<HTMLButtonElement> = {
+    onMouseOver,
+    onMouseOut,
+    onClick,
+  };
+
+  return (
+    <Button customProps={customProps} customStyle={style.css} outlined>
+      <p>{innerTextFormatter(lineType)}</p>
+    </Button>
+  );
+});
 
 const isDevEnv = process.env.NODE_ENV === "development";
 
@@ -37,53 +78,5 @@ const innerTextFormatter = (lineType: LineType) => {
     return `C${colExpected.at(1) ?? "?"}`;
   }
 };
-
-const PopoutButton = memo<Props>(function PopoutButton({ lineType }) {
-  const { languageName }       = useLanguageValue();
-  const taskData               = useTaskData();
-  const { themeName }          = useThemeValue();
-  const { tasks, layout }      = useBingoBoardValuesContext();
-  const { updateTargetedLine } = useBingoBoardActionsContext();
-
-  const repoName = taskData.repoName;
-
-  const targetTasks = getTargetTasksByLineType(tasks, lineType);
-
-  const url = `${isDevEnv ? "" : repoName}/popout`;
-  const params: PopoutQuery = {
-    tasks: targetTasks.map((v) => v.index).join(TASKS_QUERY_DELIMITER),
-    header: lineType,
-    layout: lineType === "card" ? "card" : layout,
-    lang: languageName,
-    theme: themeName,
-  };
-
-  const onClick = () => {
-    const features = CalcPopupWindowFeatures(
-      lineType === "card" ? "card" : layout,
-      taskData.size
-    );
-
-    const paramString = Object.entries(params)
-      .map(([k, v]) => `${k}=${v}`)
-      .join("&");
-    window.open(`${url}?${paramString}`, "_blank", features);
-  };
-
-  const onMouseOver = () => updateTargetedLine(lineType);
-  const onMouseOut  = () => updateTargetedLine();
-
-  const customProps: ButtonHTMLAttributes<HTMLButtonElement> = {
-    onMouseOver,
-    onMouseOut,
-    onClick,
-  };
-
-  return (
-    <Button customProps={customProps} customStyle={style.css} outlined>
-      <p>{innerTextFormatter(lineType)}</p>
-    </Button>
-  );
-});
 
 export default PopoutButton;
