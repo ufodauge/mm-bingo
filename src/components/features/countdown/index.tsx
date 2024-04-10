6940a63a13ab1e41db1b7db3136a4d86c0e5fd7f
agdfydy <hakka.ydy.sub@gmail.com>
2024-04-10 21:39:07 +0900

    fix distribute time problem

--- src/components/features/countdown/index.tsx
+++ src/components/features/countdown/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import dayjs, { Dayjs } from 'dayjs';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import CountdownUI from '@/components/ui/countdown';
 import { SEP } from '@/const/crypto';
@@ -17,38 +18,50 @@ const Countdown = () => {
     MainPageQuery
   >();
 
-  const [count, setCount] = useState<Dayjs | undefined>();
-  const [seed, setSeed]   = useState<number | undefined>();
-  const [lang, setLang]   = useState<string | undefined>();
+  const [targetTime, setTargetTime] = useState<Dayjs | undefined>();
+  const [seed, setSeed] = useState<number | undefined>();
+  const [lang, setLang] = useState<string | undefined>();
 
   const { themeName } = useThemeValue();
-
-  useInterval(() => {
-    if (count === undefined) return;
-
-    if (count.diff(undefined, "seconds", true) < 1) {
-      updateQuery("/", {
-        seed : Number(seed),
-        lang : lang ?? "en",
-        theme: themeName,
-      });
-    }
-  }, 250);
 
   useEffect(() => {
     if (!isReady) return;
 
     const { query } = getQuery();
 
-    const decrypted          = decrypt(query.code, query.key);
+    const decrypted = decrypt(query.code, query.key);
     const [seed, targetTime] = decrypted.split(SEP);
 
-    setCount(dayjs(targetTime));
     setSeed(Number(seed));
     setLang(query.lang);
+
+    axios.head(window.location.href).then((res) => {
+      const serverTime = dayjs(res.headers.date);
+      const deviceTime = dayjs();
+
+      // 1. デバイス時刻とサーバー時刻の差分を取得、
+      // 2. デバイスの時刻のほうがサーバー時刻より早ければ、
+      //    その分だけ開示を遅らせる必要がある
+      //    -> ターゲット時刻をその分だけ遅くする
+      //    -> target += server - device
+      setTargetTime(dayjs(targetTime).add(serverTime.diff(deviceTime)));
+      return undefined;
+    });
   }, [isReady, themeName]);
 
-  return <CountdownUI target={count} />;
+  useInterval(() => {
+    if (targetTime === undefined) return;
+
+    if (targetTime.diff(undefined, 'seconds', true) < 1) {
+      updateQuery('/', {
+        seed: Number(seed),
+        lang: lang ?? 'en',
+        theme: themeName,
+      });
+    }
+  }, 250);
+
+  return <CountdownUI target={targetTime} />;
 };
 
 export default Countdown;
--- src/components/ui/countdown/index.tsx
+++ src/components/ui/countdown/index.tsx
@@ -1,10 +1,10 @@
 // https://css-tricks.com/how-to-create-an-animated-countdown-timer-with-html-css-and-javascript/
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import dayjs, { Dayjs } from "dayjs";
-import { CountdownTemplate } from "./CountdownTemplate";
-import useInterval from "@/lib/hooks/useInterval";
+import dayjs, { Dayjs } from 'dayjs';
+import { CountdownTemplate } from './CountdownTemplate';
+import useInterval from '@/lib/hooks/useInterval';
 
 type Props = { target: Dayjs | undefined };
 
@@ -19,17 +19,17 @@ const Countdown: React.FC<Props> = ({ target }) => {
     return <CountdownTemplate seconds={0} text="--:--:--" />;
   }
 
-  const days    = target.diff(undefined, "days");
-  const hours   = target.diff(undefined, "hours");
-  const seconds = target.diff(undefined, "seconds");
+  const days = target.diff(undefined, 'days');
+  const hours = target.diff(undefined, 'hours');
+  const seconds = target.diff(undefined, 'seconds');
 
   const text = (() => {
     if (days > 0) {
-      return remains.format("DD [Days]");
+      return remains.format('DD [Days]');
     } else if (hours > 0) {
-      return remains.format("HH:mm:ss");
+      return remains.format('HH:mm:ss');
     }
-    return remains.format("mm:ss");
+    return remains.format('mm:ss');
   })();
 
   return <CountdownTemplate seconds={seconds} text={text} />;
